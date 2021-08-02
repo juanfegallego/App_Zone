9edf2cf9948448ee8e333c72aaa71c7fafe750de
juanfegallego <juanfegallegodj@gmail.com>
2021-08-02 19:43:56 +0200

    home structure created

---
+++ src/containers/Home/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+
+
+
+
+const Home = () => {
+
+
+    return (
+
+        <div className="vistaHome">
+            <div >
+                <h1>TESTING !</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
