d7d875fb31085add14b1d086ec6bf723777ddf63
juanfegallego <juanfegallegodj@gmail.com>
2021-08-02 19:01:44 +0200

    added variables.scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import './App.css';
+import './App.scss';
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 function App() {
@@ -8,6 +8,7 @@ function App() {
 
       <BrowserRouter>
       <Switch>
+        <Route path="/" component={Home}/>
       </Switch>   
       </BrowserRouter>
     </div>
