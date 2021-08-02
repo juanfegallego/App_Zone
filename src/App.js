452e7d642bbfd0aeb3ebbf6c502f356091969cda
juanfegallego <juanfegallegodj@gmail.com>
2021-08-02 19:41:08 +0200

    declaring the login and register paths

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 
 import './App.scss';
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import Home from './containers/Home/Home';
+import Register from './containers/Register/Register';
+import Login from './containers/Login/Login';
+
+
+
 
 function App() {
   return (
@@ -9,6 +15,9 @@ function App() {
       <BrowserRouter>
       <Switch>
         <Route path="/" component={Home}/>
+        <Route path="/register" component={Register}/> 
+        <Route path="/login" component={Login}/>
+
       </Switch>   
       </BrowserRouter>
     </div>
