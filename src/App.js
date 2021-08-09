07088d143ad94295d464e202922c0efa149f7989
juanfegallego <juanfegallegodj@gmail.com>
2021-08-09 15:24:40 +0200

    solution of errors in the routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Home from './containers/Home/Home';
 import Register from './containers/Register/Register';
 import Login from './containers/Login/Login';
+import Header from './components/Header/Header';
 
 
 
@@ -13,10 +14,11 @@ function App() {
     <div className="App">
 
       <BrowserRouter>
+      <Header/>
       <Switch>
-        <Route path="/" component={Home}/>
-        <Route path="/register" component={Register}/> 
-        <Route path="/login" component={Login}/>
+        <Route path="/" exact component={Home}/>
+        <Route path="/register" exact component={Register}/> 
+        <Route path="/login" exact component={Login}/>
 
       </Switch>   
       </BrowserRouter>
