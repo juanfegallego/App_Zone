fe29198f19fd414e5d88705bee152edd880b529b
juanfegallego <juanfegallegodj@gmail.com>
2021-08-12 15:02:26 +0200

    sample text applied

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -2,17 +2,14 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 
-
-
-
-
-
 const Home = () => {
 
 
     return (
 
-        <div className="container"></div>
+        <div className="vistaHome">
+            <div className="text1">Control de aforo para todo tipo de espacios, locales y recintos</div>
+        </div>
 
 
     )
